c32cf43609646cb0ad07236234ac1c86c24438ba
Jesse Bennett <jesse@bennettapps.com>
2023-12-08 05:51:51 +0000

    finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,75 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-["hollup", "let him cook", "caught in 4k", "random", "outta pocket"].each do |quote_type_name|
+["hollup", "let him cook", "caught in 4k", "random", "outta pocket", "ayo", "no shot"].each do |quote_type_name|
   QuoteType.find_or_create_by!(description: quote_type_name)
 end
 
-["prof stroup", "jesse", "roy", "zach", "peter", "that one guy from syria"].each do |homie_name|
+["Prof Stroup Himself", "Jesse", "Roy", "Zack", "Peter", "ThAt OnE GuY FrOm SyRiA"].each do |homie_name|
   Homie.find_or_create_by!(name: homie_name)
 end
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Roy").id,
+  quote: "Everyone in Japan looks the same",
+  quote_type: QuoteType.find_or_create_by!(description: "outta pocket"),
+  insanity: 1945,
+  star: true
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Prof Stroup Himself").id,
+  quote: "No Problem",
+  quote_type: QuoteType.find_or_create_by!(description: "random"),
+  insanity: 0,
+  star: false
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Prof Stroup Himself").id,
+  quote: "Sanity check",
+  quote_type: QuoteType.find_or_create_by!(description: "let him cook"),
+  insanity: 999,
+  star: true
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "ThAt OnE GuY FrOm SyRiA").id,
+  quote: "I'm gonna cry",
+  quote_type: QuoteType.find_or_create_by!(description: "random"),
+  insanity: -4,
+  star: false
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Zack").id,
+  quote: "You would think that",
+  quote_type: QuoteType.find_or_create_by!(description: "random"),
+  insanity: -69,
+  star: false
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Peter").id,
+  quote: "Can I please get an extension prof?",
+  quote_type: QuoteType.find_or_create_by!(description: "no shot"),
+  insanity: 420,
+  star: true
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Jesse").id,
+  quote: "Bro where's my lunchbox",
+  quote_type: QuoteType.find_or_create_by!(description: "no shot"),
+  insanity: 21,
+  star: true
+)
+
+Quote.create!(
+  homie_id: Homie.find_or_create_by!(name: "Jesse").id,
+  quote: "That's Valid",
+  quote_type: QuoteType.find_or_create_by!(description: "random"),
+  insanity: 0,
+  star: false
+)
+
